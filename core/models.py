f8394f44fa2920d4edace77f07b42d2ac39830ac
HelmiDev03 <lakhder.mohammedhelmi@istic.ucar.tn>
2023-03-25 04:32:52 +0100

    FixingDefaultProfilePicInDeployment

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 import uuid
 from datetime import datetime
+import os
 
 User=get_user_model() #curruntly authenticated user
 
@@ -10,7 +11,7 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    profileimg = models.ImageField(upload_to='profile_images' ,null=True, blank=True)
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
